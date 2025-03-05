41d815fdb84a41928dab59202f1e14a48d83a3e6
EricaThompson <EricaThompson.NC@gmail.com>
2025-03-04 21:24:49 -0500

    refactor UserActions

--- frontend/src/components/UserActions.jsx
+++ frontend/src/components/UserActions.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Post from './Post'
 
 const UserActions = () => {
+    const [newPost, setNewPost] = useState(false)
 
-    const actions = ["✍🏾","📍","🔍","✔️","🗑"]
+    const actions = [{
+        icon:"✍🏾",
+        setNewPost
+    },{
+        icon:"📍",
+        setNewPost
+    },{
+        icon:"🔍",
+        setNewPost
+    },{
+        icon:"✔️",
+        setNewPost
+    },{
+        icon:"🗑",
+        setNewPost
+    }]
     
     return (
         <div className='user-actions'>
             {actions.map((action, i)=> (
-                <div className="each-action" key={i}>
-                    {action}
+                <div onClick={()=>action.setNewPost(true)} className="each-action" key={i}>
+                    {action.icon}
                 </div>
             ))}
+            { newPost && (<Post /> )}
+            {newPost && <div className="closer" onClick={()=>setNewPost(false)}>
+                x
+            </div>}
         </div>
     )
 }
