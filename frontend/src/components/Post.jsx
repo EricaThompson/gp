3ae9b9170e18d7ff82ee675ea6221191457721d2
EricaThompson <EricaThompson.NC@gmail.com>
2025-03-04 21:25:19 -0500

    add Post and title handler

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const Post = () => {
+    const [title, setTitle] = useState();
+    const [post, setPost] = useState();
+
+    const handleTitle=(e)=>{
+        setTitle(e.target.value)
+    }
+
+    console.log(title)
+
+    const handlePost=()=>{
+
+    }
+
+  return (
+    <div className='post'>
+        <form>
+            <p>New Post</p>
+            
+            <div><label for="title">Title</label><input name="title" type="text" onChange={e => handleTitle(e)} /></div>
+            <div><label for="post">Post</label><textarea /></div>
+            <div><input type="submit" /></div>
+        </form>
+    </div>
+  )
+}
+
+export default Post
